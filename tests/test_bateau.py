7ffe955b6807e9786c88913af883208a27eb7aae
elisechengtl <tianle.cheng@centrale-med.fr>
2025-11-12 22:21:56 +0100

    Ajout Test Bateau positions

--- tests/test_bateau.py
+++ tests/test_bateau.py
@@ -13,3 +13,11 @@ def test_bateau_valeurs_par_defaut():
     assert b.colonne == 0
     assert b.longueur == 1  
     assert b.vertical is False  
+
+def test_positions_horizontal():
+    b = Bateau(2, 3, longueur=3)
+    assert b.positions == [(2, 3), (2, 4), (2, 5)]
+
+def test_positions_vertical():
+    b = Bateau(2, 3, longueur=3, vertical=True)
+    assert b.positions == [(2, 3), (3, 3), (4, 3)]
